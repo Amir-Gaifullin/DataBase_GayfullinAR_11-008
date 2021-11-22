c3f5ae30703558aa6ab58bb381772c526f35f312
Amir-Gaifullin <47780452+Amir-Gaifullin@users.noreply.github.com>
2021-11-22 10:24:04 +0300

    Update HW4.sql

--- HW4/HW4.sql
+++ HW4/HW4.sql
@@ -2,10 +2,10 @@ CREATE DATABASE hotel_hw4;
 \c hotel_hw4;
 
 CREATE TABLE hotel (
-	hotel_id INT IDENTITY PRIMARY KEY ,
+	hotel_id INT PRIMARY KEY ,
 	title VARCHAR(255) NOT NULL,
 	addres VARCHAR(255) NOT NULL,
-	category VARCHAR(255),
+	category VARCHAR(255)
 );
 
 CREATE TABLE position (
@@ -14,39 +14,39 @@ CREATE TABLE position (
 );
 
 CREATE TABLE staffer (
-	staffer_id INT IDENTITY PRIMARY KEY ,
+	staffer_id INT PRIMARY KEY ,
 	name VARCHAR(255) NOT NULL,
-	foreign_position_id INT FOREIGN KEY REFERENCES position(position_id) #one to many ,
-	foreign_hotel_id INT CONSTRAINT UNIQUE FOREIGN KEY REFERENCES #one to one
+	foreign_position_id INT FOREIGN KEY REFERENCES position(position_id),
+	foreign_hotel_id INT CONSTRAINT UNIQUE FOREIGN KEY REFERENCES
 	hotel(hotel_id)
 );
 
 CREATE TABLE room (
-	room_id INT IDENTITY PRIMARY KEY ,
+	room_id INT PRIMARY KEY ,
 	numer INT,
 	count_places INT,
 	category VARCHAR(255),
-	foreign_hotel_id INT CONSTRAINT UNIQUE FOREIGN KEY REFERENCES #one to one
+	foreign_hotel_id INT CONSTRAINT UNIQUE FOREIGN KEY REFERENCES
 );
 
 
 CREATE TABLE clean (
-	clean_id INT IDENTITY PRIMARY KEY ,
+	clean_id INT PRIMARY KEY ,
 	time TIME,
-	foreign_staffer_id INT FOREIGN KEY REFERENCES staffer(staffer_id), #one to many
+	foreign_staffer_id INT FOREIGN KEY REFERENCES staffer(staffer_id),
 	foreign_room_id INT FOREIGN KEY REFERENCES room(room_id) 
 );
 
 
 CREATE TABLE guest(
-	guest_id INT IDENTITY PRIMARY KEY
+	guest_id INT PRIMARY KEY
 	name VARCHAR(255),
 	documents VARCHAR(255),
 	contacts VARCHAR(255)
 );
 
 CREATE TABLE living (
-	living_id serial,
+	living_id INT NOT NULL,
 	period DATETIME,
 	pay INT,
 	foreign_guest_id INT FOREIGN KEY REFERENCES guest(guest_id),
