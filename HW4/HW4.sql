712825cdbb9f075e20b587d2a1b7b18aca32e5cf
Amir-Gaifullin <47780452+Amir-Gaifullin@users.noreply.github.com>
2021-11-22 10:47:15 +0300

    HW4

--- HW4/HW4.sql
+++ HW4/HW4.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE hotel_hw4;
 \c hotel_hw4;
 
+
 CREATE TABLE hotel (
 	hotel_id INT PRIMARY KEY ,
 	title VARCHAR(255) NOT NULL,
@@ -9,16 +10,18 @@ CREATE TABLE hotel (
 );
 
 CREATE TABLE position (
-	position_id INT IDENTITY PRIMARY KEY ,
+	position_id INT PRIMARY KEY ,
 	title VARCHAR(255)
 );
 
 CREATE TABLE staffer (
 	staffer_id INT PRIMARY KEY ,
 	name VARCHAR(255) NOT NULL,
-	foreign_position_id INT FOREIGN KEY REFERENCES position(position_id),
-	foreign_hotel_id INT CONSTRAINT UNIQUE FOREIGN KEY REFERENCES
-	hotel(hotel_id)
+	foreign_position_id INT,
+	FOREIGN KEY (foreign_position_id) REFERENCES position(position_id),
+	foreign_hotel_id INT,
+	FOREIGN KEY (foreign_hotel_id) REFERENCES hotel(hotel_id),
+	CONSTRAINT fk_hotel_hotel_id UNIQUE(foreign_hotel_id)
 );
 
 CREATE TABLE room (
@@ -26,20 +29,24 @@ CREATE TABLE room (
 	numer INT,
 	count_places INT,
 	category VARCHAR(255),
-	foreign_hotel_id INT CONSTRAINT UNIQUE FOREIGN KEY REFERENCES
+	foreign_hotel_id INT,
+	FOREIGN KEY (foreign_hotel_id) REFERENCES hotel(hotel_id),
+	CONSTRAINT fk_hotel_hotel_id UNIQUE(foreign_hotel_id)
 );
 
 
 CREATE TABLE clean (
 	clean_id INT PRIMARY KEY ,
 	time TIME,
-	foreign_staffer_id INT FOREIGN KEY REFERENCES staffer(staffer_id),
-	foreign_room_id INT FOREIGN KEY REFERENCES room(room_id) 
+	foreign_staffer_id INT,
+	FOREIGN KEY (foreign_staffer_id) REFERENCES staffer(staffer_id),
+	foreign_room_id INT,
+	FOREIGN KEY (foreign_room_id) REFERENCES room(room_id) 
 );
 
 
 CREATE TABLE guest(
-	guest_id INT PRIMARY KEY
+	guest_id INT PRIMARY KEY,
 	name VARCHAR(255),
 	documents VARCHAR(255),
 	contacts VARCHAR(255)
@@ -49,6 +56,8 @@ CREATE TABLE living (
 	living_id INT NOT NULL,
 	period DATETIME,
 	pay INT,
-	foreign_guest_id INT FOREIGN KEY REFERENCES guest(guest_id),
-	foreign_room_id INT FOREIGN KEY REFERENCES room(room_id)
+	foreign_guest_id INT,
+	FOREIGN KEY (foreign_guest_id) REFERENCES guest(guest_id),
+	foreign_room_id INT,
+	FOREIGN KEY (foreign_room_id) REFERENCES room(room_id)
 );
